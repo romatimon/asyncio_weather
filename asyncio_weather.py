aee1a15959455a4b4d530b0a4474396e234a3f7c
Roman Timonin <rtimonin569@gmail.com>
2024-07-05 00:19:23 +0300

    get_translation

--- asyncio_weather.py
+++ asyncio_weather.py
@@ -6,7 +6,7 @@ from aiohttp import ClientSession, web
 async def get_weather(city):
     async with ClientSession() as session:
         url = f'http://api.openweathermap.org/data/2.5/weather'
-        params = {'q': city, 'APPID': '1b7dd92t1hhh62252rb9r93fd75sbf67'}
+        params = {'q': city, 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
 
         async with session.get(url=url, params=params) as response:
             weather_json = await response.json()
@@ -16,10 +16,29 @@ async def get_weather(city):
                 return 'Нет данных'
 
 
+async def get_translation(text, source, target):
+    async with ClientSession() as session:
+        url = 'https://libretranslate.de/translate'
+
+        data = {'q': text, 'source': source, 'target': target, 'format': 'text'}
+
+        async with session.post(url, json=data) as response:
+            translate_json = await response.json()
+
+            try:
+                return translate_json['translatedText']
+            except KeyError:
+                return text
+
+
 async def handle(request):
-    city = request.rel_url.query['city']
-    weather = await get_weather(city)
-    result = {'city': city, 'weather': weather}
+    city_ru = request.rel_url.query['city']
+    city_en = await get_translation(city_ru, 'ru', 'en')
+
+    weather_en = await get_weather(city_en)
+    weather_ru = await get_translation(weather_en, 'en', 'ru')
+
+    result = {'city': city_ru, 'weather': weather_ru}
 
     return web.Response(text=json.dumps(result, ensure_ascii=False))
 
